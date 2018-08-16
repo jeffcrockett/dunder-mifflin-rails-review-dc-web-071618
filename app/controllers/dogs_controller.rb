3b01fbca44c0a42a166bf7b30114f91566bb4dfd
Jeff Crockett <jeffreyneilcrockett@gmail.com>
2018-08-16 17:36:35 -0400

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,32 @@
 class DogsController < ApplicationController
+    before_action :set_dog, only: [:show, :edit, :update]
+    before_action :get_employees, only: [:new, :edit]
+
+    def new 
+    end
+
+    def show 
+    end 
+
+    def create 
+        @dog = Dog.create(dog_params)
+        if @dog.save 
+            redirect_to dog_path(@dog)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def dog_params 
+        params.require(:dog).permit(:name, :age, :breed, employee_ids: [])
+    end
+
+    def set_dog 
+        @dog = Dog.find(params[:id])
+    end
+
+    def get_employees 
+        @employees = Employee.all
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,61 @@
 class EmployeesController < ApplicationController
+     before_action :set_employee, only: [:show, :edit, :update, :destroy]
+     before_action :get_dogs, only: [:new, :edit]
+
+     def index 
+        @employees = Employee.all
+     end
+
+     def show 
+     end
+
+     def new 
+        @employee = Employee.new
+     end
+
+     def create 
+        @employee = Employee.create(employee_params)
+        # binding.pry
+        if @employee.save 
+            redirect_to employee_path(@employee)
+        else
+            render :new 
+        end
+    end
+
+     def edit 
+     end
+
+     def update 
+        @employee.update(employee_params)
+        if @employee.save 
+            redirect_to employee_path(@employee)
+        else
+            render :edit
+        end
+
+     end
+
+     def destroy 
+        @employee.destroy
+        redirect_to employees_path
+     end
+        
+
+    private 
+
+    def employee_params 
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id, :img_url)
+    end
+
+    def set_employee 
+        @employee = Employee.find(params[:id])
+    end
+
+    def get_dogs
+        @dogs = Dog.all 
+    end
+
+   
+    
 end
